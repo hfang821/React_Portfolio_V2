2ab37c9376bdfda9976862dbd44b64278d746fd8
hfang821 <95199209+hfang821@users.noreply.github.com>
2022-09-01 00:30:13 -0400

    add emailjs for the contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -4,12 +4,13 @@ import Row from 'react-bootstrap/esm/Row';
 import Col from 'react-bootstrap/esm/Col';
 import {Box, Card, CardContent, CardActions, Typography, Button, CardMedia, Stack, Chip} from '@mui/material';
 import { validateEmail } from '../utils/helpers';
-
+import emailjs from 'emailjs-com';
 
 export default function Project({ color, projectInfo, num, theme }) {
     const [email, setEmail] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
     const [Name, setName] = useState('');
+    const [Subject, setSubject] = useState('');
   
     const handleInputChange = (e) => {
       // Getting the value and name of the input which triggered the change
@@ -19,9 +20,11 @@ export default function Project({ color, projectInfo, num, theme }) {
   
       if (inputType === 'email') {
         setEmail(inputValue);
+      } else if (inputType === 'subject'){
+        setSubject(inputValue);
       } else {
         setName(inputValue);
-      } 
+      }
     };
   
   
@@ -36,13 +39,27 @@ export default function Project({ color, projectInfo, num, theme }) {
       }
       setName('');
       setEmail('');
-      alert(`Hello ${Name}`);
+      alert(`Hello ${Name}, I will return to your message soon!`);
+
+      emailjs.sendForm('service_ychkaw9', 'template_wmvj3um', e.target, 'nNDcTXGjx8wGrJLqN')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
+
     };
+
   
     return(
         <div className="contact">
     <h1 style={{color: 'white', marginBottom: '34px', marginLeft: '2%'}}>Contact Me</h1>
-  <form>
+  <form onSubmit={handleFormSubmit}>
+      <div class="mb-3" style={{width:"41%", marginLeft:"30%"}}>
+        <label for="exampleInputPassword1" class="form-label" style={{color: 'skyblue'}}>Subject</label>
+        <input type="subject" class="form-control" name="subject" value={Subject} onChange={handleInputChange}/>
+      </div>
       <div class="mb-3" style={{width:"41%", marginLeft:"30%"}}>
         <label for="exampleInputPassword1" class="form-label" style={{color: 'skyblue'}}>Name</label>
         <input type="name" class="form-control" name="name" value={Name} onChange={handleInputChange}/>
@@ -59,10 +76,10 @@ export default function Project({ color, projectInfo, num, theme }) {
         )}
       <div class="mb-3" style={{width:"41%", marginLeft:"30%"}}>
         <label for="exampleFormControlTextarea1" class="form-label" style={{color: 'skyblue'}}>Message: </label>
-        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"/>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="message"/>
       </div>
 
-      <button type="submit" class="btn btn-primary" onClick={handleFormSubmit} style={{backgroundColor: 'LightSeaGreen', marginTop:'2%'}}>Submit</button>
+      <button type="submit" class="btn btn-primary" style={{backgroundColor: 'LightSeaGreen', marginTop:'2%'}}>Submit</button>
   </form>
 
 </div>
